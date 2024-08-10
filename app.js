06cc963531a41cd8f89e6292783fc1553a8678f0
ranjitcj <ranjitcjagtap@gmail.com>
2024-08-10 22:07:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 10000;
 
 // Proxy requests to the PHP server
 app.use("/", createProxyMiddleware({

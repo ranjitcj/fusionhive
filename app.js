e66c017bb3ffe37602a543f2c32d81d2c3ab7a90
ranjitcj <ranjitcjagtap@gmail.com>
2024-08-10 22:39:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { exec } = require('child_process');
 const app = express();
-const port = 8080;
+const port = 8000;
 
 app.get('/', (req, res) => {
   exec('php-cgi index.php', (error, stdout, stderr) => {

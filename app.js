49b1b93100e88b4a4cf6b2dd91506cf298f07f4a
ranjitcj <ranjitcjagtap@gmail.com>
2024-08-10 20:53:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 8000;
 
 // Serve static files from the "fusionhive" directory
 app.use(express.static(path.join(__dirname, 'fusionhive')));

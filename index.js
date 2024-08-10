a0b4a0595b0744473192fca63d319c2e00b50542
ranjitcj <ranjitcjagtap@gmail.com>
2024-08-10 22:24:38 +0530

    new

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { exec } = require('child_process');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  exec('php-cgi index.php', (error, stdout, stderr) => {
+    if (error) {
+      res.status(500).send('Error executing PHP script');
+      return;
+    }
+    res.send(stdout);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Node.js server running at http://localhost:${port}`);
+});
